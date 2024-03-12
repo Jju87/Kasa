9df3898b6c1a51bb422160cc3e911d67b4bb1b9e
Julien <gardier.julien@gmail.com>
2024-03-12 01:01:37 +0100

    about page implemented

---
+++ src/components/Collapse/Collapse.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import '../Collapse/Collapse.scss';
+import arrow from '../../assets/images/vectorHaut.svg';
+
+function Collapse({ text, title }) {
+    // Utilise le hook useState pour gérer l'état de l'élément Collapse et la classe CSS
+    const [isCollapsed, setIsCollapsed] = useState(true);
+
+    // Crée une fonction pour basculer l'état de l'élément Collapse et mettre à jour la classe CSS
+    const toggleCollapse = () => {
+        setIsCollapsed(!isCollapsed);
+    };
+
+    // Détermine la classe CSS en fonction de l'état
+    const contentClass = isCollapsed ? "hidden" : "visible";
+    const buttonImgClass = !isCollapsed ? "rotate" : ""; 
+
+    // Renvoie un élément Collapse avec un bouton pour basculer l'état de l'élément et un élément p pour afficher le contenu
+    return (
+        <article className="collapse-container">
+            <button onClick={toggleCollapse}>{title} <img className={buttonImgClass} src={arrow} alt="white arrow"></img></button>
+            {/* Utilise la classe CSS déterminée dynamiquement */}
+            <p className={`collapse-content ${contentClass}`}>
+                {text}
+            </p>
+        </article>
+    );
+}
+
+export default Collapse;
--- src/pages/A-propos/A-propos.jsx
+++ src/pages/A-propos/A-propos.jsx
@@ -1,19 +1,40 @@
-import {useEffect} from 'react';
+import { useEffect } from "react";
+import React from "react";
+import bannerAbout from "../../assets/images/bg-about_mountain.png";
+import "../A-propos/A-propos.scss";
+import Collapse from "../../components/Collapse/Collapse";
 
 function APropos() {
-  // Utilise le hook useEffect pour mettre à jour le titre de l'onglet
-  useEffect(() => {
-    document.title = `A propos - Kasa`;
-  }, []);
-  
-  return (
-    <div>
-      <h1>A propos</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
-      </p>
-    </div>
-  )
+    // Utilise le hook useEffect pour mettre à jour le titre de l'onglet
+    useEffect(() => {
+        document.title = `A propos - Kasa`;
+    }, []);
+
+    return (
+        <div className="about-container">
+            <div className="banner-about">
+                <img src={bannerAbout} alt="Vue sur des sommets de montagne" />
+            </div>
+            <section>
+                <Collapse
+                    title="Fiabilité"
+                    text="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+                />
+                <Collapse
+                    title="Respect"
+                    text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+                />
+                <Collapse
+                    title="Service"
+                    text="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+                />
+                <Collapse
+                    title="Sécurité"
+                    text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+                />
+            </section>
+        </div>
+    );
 }
 
 export default APropos;
