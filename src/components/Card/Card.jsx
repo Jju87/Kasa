efe6c5996ce912a6eea5114d6e74ee9314d1f277
Julien <gardier.julien@gmail.com>
2024-03-13 01:25:53 +0100

    home page completed and styled

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,12 @@
+import  '../Card/Card.scss'
+
+function Card({ cover, title }) {
+    return (
+        <article className="cards-gallery__card">
+            <img className='cards-gallery__card--cover' src={cover} alt={title} />
+            <span className='cards-gallery__card--title'>{title}</span>
+        </article>
+    );
+}
+
+export default Card;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,33 +1,52 @@
 import { Link } from "react-router-dom";
-import { useEffect } from 'react';
-import bannerHome from '../../assets/images/bg-banner_forest.png'
-import '../Home/Home.scss';
-
+import { useEffect, useState } from "react";
+import bannerHome from "../../assets/images/bg-banner_forest.png";
+import "../Home/Home.scss";
+import Card from "../../components/Card/Card";
 
 function Home() {
-    const LocationNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     // Utilise le hook useEffect pour mettre à jour le titre de l'onglet
     useEffect(() => {
-      document.title = `Acceuil - Kasa`;
+        document.title = `Acceuil - Kasa`;
     }, []);
-  return (
-    <div className="home-container">
-      <div className="banner-home">
-        <img src={bannerHome} alt="Vue sur une forêt" />
-      </div>
-      <h1>Home</h1>
-      <article>
-        <Link to={`/fiche-logement/${LocationNumber[0]}`}>
-            Ici il y aura les cards des logements
-        </Link>
-        <Link to={`/fiche-logement/${LocationNumber[1]}`}>
-            Ici il y aura les cards des logements
-        </Link>
-      
-      </article>
 
-    </div>
-  );
+    // Génération du useState avec la valeur initiale null
+    const [data, setData] = useState([]);
+
+    // Utilise le hook useEffect pour récupérer les données de l'API
+    useEffect(() => {
+        const fetchData = async () => {
+            // Simule une requête vers une API avec process.env.PUBLIC_URL
+            const response = await fetch(
+                process.env.PUBLIC_URL + "/rentals.json"
+            );
+            const jsonResponse = await response.json();
+            // Met à jour de l'état avec les données de l'API
+            setData(jsonResponse);
+        };
+        // Appel de la fonction fetchData
+        fetchData();
+    }, []);
+
+    return (
+        <div className="home-container">
+            <div className="banner-home">
+                <img src={bannerHome} alt="Vue sur une forêt" />
+            </div>
+            <section className="cards-gallery">
+                {data.map((rental) => {
+                    return (
+                        <Link
+                            to={`/Fiche-Logement/${rental.id}`}
+                            key={rental.id}
+                        >
+                            <Card cover={rental.cover} title={rental.title} />
+                        </Link>
+                    );
+                })}
+            </section>
+        </div>
+    );
 }
 
 export default Home;
